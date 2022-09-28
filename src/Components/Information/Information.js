facb67cba2ceb93adef33db1d15f7ffd43b8e95a
swapon789 <kumarshopon850@gmail.com>
2022-09-28 21:06:13 +0600

    bootstrap link added

---
+++ src/Components/Information/Information.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Information = () => {
+    return (
+        <div>
+            <div className="information">
+
+            </div>
+            <div className="activity">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Information;
