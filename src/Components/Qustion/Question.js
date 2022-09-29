119e3558266cbf990e46e6abed0d3caad6c49ad2
swapon789 <kumarshopon850@gmail.com>
2022-09-29 16:38:46 +0600

    2 accordian section added

--- src/Components/Qustion/Question.js
+++ src/Components/Qustion/Question.js
@@ -4,7 +4,39 @@ const Question = () => {
     return (
         <div className='container mt-5 '>
             <h2 className='text-center'>Question? And Answer.</h2>
-            
+            {/* accordian section start*/}
+            <div className="accordion my-3" id="accordionExample">
+                {/* first accordian */}
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                        <button className="accordion-button text-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            How Does React Work?
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>React:</strong>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.
+                        </div>
+                    </div>
+                </div>
+                {/* second accordian */}
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed text-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            How difference between props and state in react?
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            Props:
+                            <p>Props are read-only components. It is an object which stores the value of attributes of a tag and work similar to the HTML attributes. It allows passing data from one component to other components. It is similar to function arguments and can be passed to the component the same way as arguments passed in a function.</p>
+                            State:
+                            <p>The state is an updatable structure that is used to contain data or information about the component and can change over time. The change in state can happen as a response to user action or system event. It is the heart of the react component which determines the behavior of the component and how it will render. A state must be kept as simple as possible.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* accordian section end*/}
         </div>
     );
 };
